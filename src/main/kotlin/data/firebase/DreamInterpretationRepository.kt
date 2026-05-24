85899397c1ffe2c99e9a1539bda97b1771dc90d2
neldroid <nahuelul@gmail.com>
2026-05-24 19:00:46 +0200

    Change repositories

--- src/main/kotlin/data/firebase/DreamInterpretationRepository.kt
+++ src/main/kotlin/data/firebase/DreamInterpretationRepository.kt
@@ -1,9 +1,19 @@
 package data.firebase
 
+import com.google.cloud.Timestamp
+import com.google.cloud.firestore.FieldValue
 import com.google.cloud.firestore.Firestore
 import common.model.dream.LLMDreamInterpretation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.plus
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 
 class DreamInterpretationRepository(firestore: Firestore) : FirestoreRepository<LLMDreamInterpretation>(
     firestore = firestore,
@@ -17,6 +27,11 @@ class DreamInterpretationRepository(firestore: Firestore) : FirestoreRepository<
         interpretation: LLMDreamInterpretation,
     ) = withContext(Dispatchers.IO) {
         subCollection(userId, "dreams").document(interpretationId).set(interpretation)
+
+        update(userId, mapOf(
+            "dream.readingsAmount" to FieldValue.increment(-1),
+            "dream.nextFreeReadingAt" to nextFreeReadingDate(),
+        ))
     }
 
     suspend fun getLastInterpretationSummaries(userId: String, limit: Int = 3): List<String> =
@@ -27,4 +42,5 @@ class DreamInterpretationRepository(firestore: Firestore) : FirestoreRepository<
                 .documents
                 .mapNotNull { it.toObject(LLMDreamInterpretation::class.java).summary }
         }
+
 }
